a8fbccd6ff504a3a1733995b782d4b6ec3405b63
Macoy Madson <macoymadson@gmail.com>
2020-03-13 19:18:20 -0700

    More SFML stuff

--- BuildDependencies_Debug.sh
+++ BuildDependencies_Debug.sh
@@ -6,7 +6,10 @@ echo "WARNING: This will run git clean -f on SFML"
 cd dependencies/SFML
 make clean
 git clean -f
-cmake . -DCMAKE_BUILD_TYPE=Debug
+cmake . -DCMAKE_BUILD_TYPE=Debug \
+	  -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++14 -stdlib=libstdc++" \
+	  -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libstdc++" \
+	  -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE -DCMAKE_VERBOSE_MAKEFILE=TRUE
 make -j4
 
 cd ../../
